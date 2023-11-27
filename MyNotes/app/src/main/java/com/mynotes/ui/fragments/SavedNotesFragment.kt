a7b63aa8c41c81e356bfa8b9db29d0b276b360ee
Michal Wesolowski <michal.wesolov@gmail.com>
2023-11-27 13:26:51 +0100

    Implemented saved notes

--- MyNotes/app/src/main/java/com/mynotes/ui/fragments/SavedNotesFragment.kt
+++ MyNotes/app/src/main/java/com/mynotes/ui/fragments/SavedNotesFragment.kt
@@ -3,15 +3,35 @@ package com.mynotes.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mynotes.R
+import com.mynotes.adapters.NotesAdapter
 import com.mynotes.ui.MainActivity
 import com.mynotes.ui.NotesViewModel
 
 class SavedNotesFragment: Fragment(R.layout.fragment_saved_notes) {
 
     lateinit var viewModel: NotesViewModel
+    lateinit var notesAdapter: NotesAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+        initRecycleView()
+
+        viewModel.getFavoritedNotes().observe(viewLifecycleOwner, Observer {notesList ->
+            notesAdapter.differ.submitList(notesList)
+        })
+    }
+
+    private fun initRecycleView() {
+        notesAdapter = NotesAdapter()
+        val rvLocalNotes = view?.findViewById<RecyclerView>(R.id.rvSavedNotes)
+        rvLocalNotes?.apply {
+            adapter = notesAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
