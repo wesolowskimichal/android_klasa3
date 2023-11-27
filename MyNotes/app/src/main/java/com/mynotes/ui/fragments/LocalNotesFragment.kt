3c206fabc68040d5a3b8f10d7e8104e9d1a048c9
Michal Wesolowski <michal.wesolov@gmail.com>
2023-11-27 21:44:09 +0100

    implemented creating new notes and adding/deleting to/from favorites

--- MyNotes/app/src/main/java/com/mynotes/ui/fragments/LocalNotesFragment.kt
+++ MyNotes/app/src/main/java/com/mynotes/ui/fragments/LocalNotesFragment.kt
@@ -1,12 +1,15 @@
 package com.mynotes.ui.fragments
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Note
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mynotes.R
 import com.mynotes.adapters.NotesAdapter
 import com.mynotes.ui.MainActivity
@@ -25,6 +28,21 @@ class LocalNotesFragment: Fragment(R.layout.fragment_local_notes) {
         notesAdapter.setOnItemClickListener {
             val bundle = Bundle().apply {
                 putSerializable("note", it)
+            }
+
+            findNavController().navigate(
+                R.id.action_localNotesFragment_to_noteFragment,
+                bundle
+            )
+        }
+
+        val addNoteButton = view.findViewById<FloatingActionButton>(R.id.fab_addNote)
+        addNoteButton.setOnClickListener {
+            // Create a new note instance and navigate to the NoteFragment
+            val newNote =
+                com.mynotes.models.Note(title = "New Note", content = " ", modificationDate = "19.09.1999", isFavorited = false) // Replace Note() with your actual Note class instantiation logic
+            val bundle = Bundle().apply {
+                putSerializable("note", newNote)
             }
 
             findNavController().navigate(
--- MyNotes/app/src/main/java/com/mynotes/ui/fragments/NoteFragment.kt
+++ MyNotes/app/src/main/java/com/mynotes/ui/fragments/NoteFragment.kt
@@ -7,6 +7,8 @@ import android.widget.EditText
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 import com.mynotes.R
 import com.mynotes.ui.MainActivity
 import com.mynotes.ui.NotesViewModel
@@ -23,6 +25,7 @@ class NoteFragment: Fragment(R.layout.fragment_note) {
         val title = view.findViewById<EditText>(R.id.etTitle)
         val context = view.findViewById<EditText>(R.id.etContext)
         val save = view.findViewById<Button>(R.id.bSaveNote)
+        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
         title.setText(note.title)
         context.setText(note.content)
         title.addTextChangedListener {
@@ -33,6 +36,16 @@ class NoteFragment: Fragment(R.layout.fragment_note) {
         }
         save.setOnClickListener {
             viewModel.saveNote(note)
+        }
+        fab.setOnClickListener {
+            if(note.id != null) {
+                viewModel.updateIsFavorited(note.id!!, !note.isFavorited)
+                if(note.isFavorited) {
+                    Snackbar.make(view, "Note deleted from favorited", Snackbar.LENGTH_SHORT).show()
+                } else {
+                    Snackbar.make(view, "Note added to favorited", Snackbar.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
